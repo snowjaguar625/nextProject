c5d3cdf08a7813cfeb69eb915cb6362e88b0f704
BonJ <snowjaguar625@gmail.com>
2020-10-02 11:57:11 -0300

    Adding real images

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,32 +1,51 @@
-import cn from "classnames";
-import s from "./ProductCard.module.css";
-import React, { FunctionComponent } from "react";
-import { Heart } from "@components/icon";
+import cn from 'classnames'
+import s from './ProductCard.module.css'
+import { FC } from 'react'
+import { Heart } from '@components/icon'
 interface Props {
-  className?: string;
-  children?: any;
+  className?: string
+  children?: any
+  productData: ProductData
 }
 
-const ProductCard: FunctionComponent<Props> = ({ className }) => {
-  const rootClassName = cn(s.root, className);
+interface ProductData {
+  name: string
+  images: any
+}
+
+const ProductCard: FC<Props> = ({ className, productData }) => {
+  const rootClassName = cn(s.root, className)
+  console.log(productData)
   return (
     <div className={rootClassName}>
-      <div className="absolute">
-        <h1 className="px-8 py-2 bg-white text-black font-bold text-3xl">
-          T-Shirt
-        </h1>
-        <div className="px-6 py-2 pb-4 bg-white text-black font-semibold inline-block">
-          $50
+      {/* Overlay */}
+      <div className="flex flex-row justify-between box-border w-full z-10 relative">
+        <div className="flex flex-col">
+          <div>
+            <h1 className="p-3 h-14 bg-white text-black font-bold text-2xl truncate leading-8">
+              {productData.name}
+            </h1>
+          </div>
+          <div>
+            <div className="px-6 py-1 pb-3 bg-white text-black font-semibold inline-block text-sm leading-6">
+              $50
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center h-12 w-12 bg-white text-black cursor-pointer">
+          <Heart />
         </div>
       </div>
-      <div className="absolute flex items-center justify-center h-12 w-12 bg-white text-black">
-        <Heart />
-      </div>
-      <div className="flex-1 h-full p-24">
-        <div className="bg-violet h-full"></div>
+
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <img
+          className="object-cover object-center w-full"
+          src={productData.images.edges[0].node.urlSmall}
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductCard;
+export default ProductCard
