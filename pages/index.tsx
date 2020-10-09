3e0ff99c5fbf786fc6e8cb96f7b617378f863896
BonJ <snowjaguar625@gmail.com>
2020-10-08 18:00:05 -0600

    Styling and icon fixes for cart sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,17 @@ import getAllProducts from '@lib/bigcommerce/api/operations/get-all-products'
 import { Layout } from '@components/core'
 import { Grid, Marquee, Hero } from '@components/ui'
 import { ProductCard } from '@components/product'
-import getSiteInfo from '@lib/bigcommerce/api/operations/get-site-info'
 
 export async function getStaticProps({ preview }: GetStaticPropsContext) {
   const { products } = await getAllProducts()
-  const { categories } = await getSiteInfo()
 
   return {
-    props: { products, categories },
+    props: { products },
   }
 }
 
 export default function Home({
   products,
-  categories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
@@ -41,18 +38,9 @@ export default function Home({
         variant="secondary"
         wrapper={(p: any) => <ProductCard {...p} variant="slim" />}
       />
-      <div className="py-12 flex flex-row w-full">
-        <div className="pr-3 w-48">
-          <ul className="uppercase">
-            <li>
-              <h2 className="font-bold">All Categories</h2>
-            </li>
-            {categories.map((cat) => (
-              <li key={cat.path} className="mt-2">
-                <a href="#">{cat.name}</a>
-              </li>
-            ))}
-          </ul>
+      <div className="py-12 px-4 flex flex-row w-full">
+        <div className="flex-0 pr-3 w-48 break-words">
+          ALL CATEGORIES ACCESSORIES BAGS CLOTHING SHOES ALL DESIGNERS 032c 1017
         </div>
         <div className="flex-1">
           <Grid
