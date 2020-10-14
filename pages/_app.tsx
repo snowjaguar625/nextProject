5cfe6a9d5fb4b944bceb49449f4d0066f0e027a1
BonJ <snowjaguar625@gmail.com>
2020-10-14 11:50:38 -0500

    Added sort option to the UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,25 @@
 import { FC } from 'react'
-import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
 import { SSRProvider, OverlayProvider } from 'react-aria'
+import { ThemeProvider } from 'next-themes'
 import '@assets/global.css'
 import '@assets/tailwind.css'
 import '@assets/utils.css'
-import config from '../config.json'
+
 const Noop: FC = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
 
   return (
-    <>
-      <DefaultSeo {...config.seo} />
-      <ThemeProvider>
-        <SSRProvider>
-          <OverlayProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </OverlayProvider>
-        </SSRProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <SSRProvider>
+        <OverlayProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </OverlayProvider>
+      </SSRProvider>
+    </ThemeProvider>
   )
 }
