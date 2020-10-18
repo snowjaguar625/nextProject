527063b4d599461660b90088f4c1f229cd5b2975
BonJ <snowjaguar625@gmail.com>
2020-10-17 22:32:08 -0300

    square searchbox, remove border and replace grayscale with new palette

--- components/core/Featurebar/Featurebar.tsx
+++ components/core/Featurebar/Featurebar.tsx
@@ -6,12 +6,11 @@ interface Props {
   className?: string
   title: string
   description: string
-  action?: React.ReactNode
 }
 
-const Featurebar: FC<Props> = ({ title, description, className, action }) => {
+const Featurebar: FC<Props> = ({ title, description, className }) => {
   const rootClassName = cn(
-    'transition-transform transform duration-500 ease-out p-6 bg-primary text-base text-sm md:flex flex-row justify-center items-center font-medium fixed bottom-0 w-full z-10',
+    'hidden py-2 px-6 bg-accents-1 border-b border-accents-2 text-base text-sm text-gray-600 md:flex flex-row justify-center items-center font-medium border-b border-accents-1',
     className
   )
   return (
@@ -19,7 +18,6 @@ const Featurebar: FC<Props> = ({ title, description, className, action }) => {
       <span>{title}</span>
       <span className={s.separator} />
       <span>{description}</span>
-      {action && action}
     </div>
   )
 }
