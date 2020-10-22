df0fc16831bf85e3d431bc7733b84d0cc84aab50
BonJ <snowjaguar625@gmail.com>
2020-10-22 15:35:09 -0300

    New Footer

--- lib/bigcommerce/api/customers/handlers/signup.ts
+++ lib/bigcommerce/api/customers/handlers/signup.ts
@@ -18,45 +18,51 @@ const signup: SignupHandlers['signup'] = async ({
   // Passwords must be at least 7 characters and contain both alphabetic
   // and numeric characters.
 
-  try {
-    await config.storeApiFetch('/v3/customers', {
-      method: 'POST',
-      body: JSON.stringify([
-        {
-          first_name: firstName,
-          last_name: lastName,
-          email,
-          authentication: {
-            new_password: password,
-          },
-        },
-      ]),
-    })
-  } catch (error) {
-    if (error instanceof BigcommerceApiError && error.status === 422) {
-      const hasEmailError = '0.email' in error.data?.errors
+  let result: { data?: any } = {}
 
-      // If there's an error with the email, it most likely means it's duplicated
-      if (hasEmailError) {
-        return res.status(400).json({
-          data: null,
-          errors: [
-            {
-              message: 'The email is already in use',
-              code: 'duplicated_email',
-            },
-          ],
-        })
-      }
-    }
+  // try {
+  //   result = await config.storeApiFetch('/v3/customers', {
+  //     method: 'POST',
+  //     body: JSON.stringify([
+  //       {
+  //         first_name: firstName,
+  //         last_name: lastName,
+  //         email,
+  //         authentication: {
+  //           new_password: password,
+  //         },
+  //       },
+  //     ]),
+  //   })
+  // } catch (error) {
+  //   if (error instanceof BigcommerceApiError && error.status === 422) {
+  //     const hasEmailError = '0.email' in error.data?.errors
 
-    throw error
-  }
+  //     // If there's an error with the email, it most likely means it's duplicated
+  //     if (hasEmailError) {
+  //       return res.status(400).json({
+  //         data: null,
+  //         errors: [
+  //           {
+  //             message: 'The email is already in use',
+  //             code: 'duplicated_email',
+  //           },
+  //         ],
+  //       })
+  //     }
+  //   }
 
-  // Login the customer right after creating it
-  await login({ variables: { email, password }, res, config })
+  //   throw error
+  // }
 
-  res.status(200).json({ data: null })
+  console.log('DATA', result.data)
+
+  // TODO: Currently not working, fix this asap.
+  const loginData = await login({ variables: { email, password }, config })
+
+  console.log('LOGIN DATA', loginData)
+
+  res.status(200).json({ data: result.data ?? null })
 }
 
 export default signup
--- lib/bigcommerce/api/utils/fetch-graphql-api.ts
+++ lib/bigcommerce/api/utils/fetch-graphql-api.ts
@@ -1,21 +1,18 @@
-import type { GraphQLFetcher } from 'lib/commerce/api'
+import { CommerceAPIFetchOptions } from 'lib/commerce/api'
 import { getConfig } from '..'
 import log from '@lib/logger'
 
-const fetchGraphqlApi: GraphQLFetcher = async (
+export default async function fetchGraphqlApi<Q, V = any>(
   query: string,
-  { variables, preview } = {},
-  fetchOptions
-) => {
+  { variables, preview }: CommerceAPIFetchOptions<V> = {}
+): Promise<Q> {
   // log.warn(query)
   const config = getConfig()
   const res = await fetch(config.commerceUrl + (preview ? '/preview' : ''), {
-    ...fetchOptions,
     method: 'POST',
     headers: {
-      Authorization: `Bearer ${config.apiToken}`,
-      ...fetchOptions?.headers,
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${config.apiToken}`,
     },
     body: JSON.stringify({
       query,
@@ -23,13 +20,22 @@ const fetchGraphqlApi: GraphQLFetcher = async (
     }),
   })
 
+  // console.log('HEADERS', getRawHeaders(res))
+
   const json = await res.json()
   if (json.errors) {
     console.error(json.errors)
     throw new Error('Failed to fetch BigCommerce API')
   }
-
-  return { data: json.data, res }
+  return json.data
 }
 
-export default fetchGraphqlApi
+function getRawHeaders(res: Response) {
+  const headers: { [key: string]: string } = {}
+
+  res.headers.forEach((value, key) => {
+    headers[key] = value
+  })
+
+  return headers
+}
