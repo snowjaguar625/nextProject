7d521e879defd5d846983788b16a950481120a73
BonJ <snowjaguar625@gmail.com>
2020-10-23 19:47:10 -0300

    general improvements

--- components/icon/Moon.tsx
+++ components/icon/Moon.tsx
@@ -5,11 +5,11 @@ const Moon = ({ ...props }) => {
       width="24"
       height="24"
       stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       fill="none"
-      shapeRendering="geometricPrecision"
+      shape-rendering="geometricPrecision"
       {...props}
     >
       <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
