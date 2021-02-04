0f6b4cad2d92a43df6de6fffd7b0d12038bcd3cf
BonJ <snowjaguar625@gmail.com>
2021-02-04 12:29:20 -0300

    userAvatar

--- framework/shopify/utils/mutations/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as checkoutCreateMutation } from './checkout-create'
-export { default as checkoutLineItemAddMutation } from './checkout-line-item-add'
-export { default as checkoutLineItemUpdateMutation } from './checkout-create'
-export { default as checkoutLineItemRemoveMutation } from './checkout-line-item-remove'
