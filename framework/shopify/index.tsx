81028918cbbf18253804f77d36fa75d93d9017d3
BonJ <snowjaguar625@gmail.com>
2021-02-09 09:14:42 +0200

    change readme

---
+++ framework/shopify/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { ReactNode } from 'react'
+
+import {
+  CommerceProvider as CoreCommerceProvider,
+  useCommerce as useCoreCommerce,
+} from '@commerce'
+
+import shopifyConfig, { ShopifyConfig } from './config'
+export type ShopifyProps = {
+  children?: ReactNode
+  locale: string
+} & ShopifyConfig
+
+export function CommerceProvider({ children, ...config }: ShopifyProps) {
+  return (
+    <CoreCommerceProvider config={{ ...shopifyConfig, ...config }}>
+      {children}
+    </CoreCommerceProvider>
+  )
+}
+
+export const useCommerce = () => useCoreCommerce()
